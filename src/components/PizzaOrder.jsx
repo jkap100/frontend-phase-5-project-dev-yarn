df511848b85268abe5190a625a63f0ea0c420b84
Jon Kaplon <jon.kaplon1@gmail.com>
2022-05-20 11:15:45 -0700

    veggies

--- src/components/PizzaOrder.jsx
+++ src/components/PizzaOrder.jsx
@@ -59,42 +59,46 @@ function PizzaOrder({
   const sauceName = !sauceOrder ? "" : sauceOrder.name;
 
   const meatNames =
-    meatsOrder === undefined || meatsOrder.length == 0
-      ? ""
-      : meatsOrder.map((meat) => {
-          return (
-            <motion.li
-              key={meat.id}
-              whileHover={{
-                scale: 1.3,
-                originX: 0,
-                color: "#f8e112",
-              }}
-              transition={{ type: "spring", stiffness: 300 }}
-            >
-              {meat.name}
-            </motion.li>
-          );
-        });
+    meatsOrder === undefined || meatsOrder.length == 0 ? (
+      <li>Select Meats</li>
+    ) : (
+      meatsOrder.map((meat) => {
+        return (
+          <motion.li
+            key={meat.id}
+            whileHover={{
+              scale: 1.3,
+              originX: 0,
+              color: "#f8e112",
+            }}
+            transition={{ type: "spring", stiffness: 300 }}
+          >
+            {meat.name}
+          </motion.li>
+        );
+      })
+    );
 
   const veggieNames =
-    veggiesOrder === undefined || veggiesOrder.length == 0
-      ? ""
-      : veggiesOrder.map((veggie) => {
-          return (
-            <motion.li
-              key={veggie.id}
-              whileHover={{
-                scale: 1.3,
-                originX: 0,
-                color: "#f8e112",
-              }}
-              transition={{ type: "spring", stiffness: 300 }}
-            >
-              {veggie.name}
-            </motion.li>
-          );
-        });
+    veggiesOrder === undefined || veggiesOrder.length == 0 ? (
+      <li>Select Veggies</li>
+    ) : (
+      veggiesOrder.map((veggie) => {
+        return (
+          <motion.li
+            key={veggie.id}
+            whileHover={{
+              scale: 1.3,
+              originX: 0,
+              color: "#f8e112",
+            }}
+            transition={{ type: "spring", stiffness: 300 }}
+          >
+            {veggie.name}
+          </motion.li>
+        );
+      })
+    );
 
   const startOver = () => {
     navigate("/crust");
@@ -124,7 +128,7 @@ function PizzaOrder({
           }}
           transition={{ type: "spring", stiffness: 300 }}
         >
-          {crustName}
+          {!crustName ? "Select A Crust" : crustName}
         </motion.li>
         <li>
           <strong className="underline has-text-white">Sauce:</strong>
@@ -137,7 +141,7 @@ function PizzaOrder({
           }}
           transition={{ type: "spring", stiffness: 300 }}
         >
-          {sauceName}
+          {!sauceName ? "Select A Sauce" : sauceName}
         </motion.li>
         <li className="">
           <strong className="underline has-text-white">Meats:</strong>
