da26c3b2ffb00459a29205a347d5b6348d7ca1eb
Jon Kaplon <jon.kaplon1@gmail.com>
2022-05-27 08:23:22 -0700

    map

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -8,17 +8,19 @@ import {
   Marker,
   InfoWindow,
 } from "@react-google-maps/api";
-// import usePlacesAutocomplete, {
-//   getGeocode,
-//   getLatLng,
-// } from "use-places-autocomplete";
-// import {
-//   Combobox,
-//   ComboboxInput,
-//   ComboboxPopover,
-//   ComboboxList,
-//   ComboboxOption,
-// } from "@reach/combobox";
+
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from "@reach/combobox";
+
 import { formatRelative } from "date-fns";
 import "@reach/combobox/styles.css";
 import MapStyles from "../components/MapStyles";
@@ -172,6 +174,7 @@ export default function Map({
             animate="visible"
             exit="exit"
           >
+            <Search panTo={panTo} />
             <GoogleMap
               mapContainerStyle={mapContainerStyle}
               zoom={11}
@@ -285,5 +288,57 @@ export default function Map({
         </div>
       </div>
     </motion.div>
+  );
+}
+
+function Search({ panTo }) {
+  const {
+    ready,
+    value,
+    suggestions: { status, data },
+    setValue,
+    clearSuggestions,
+  } = usePlacesAutocomplete({
+    requestOptions: {
+      location: { lat: () => 45.5008182, lng: () => -122.6683848 },
+      radius: 200 * 1000,
+    },
+  });
+
+  return (
+    <div className="search">
+      <Combobox
+        onSelect={async (address) => {
+          setValue(address, false);
+          clearSuggestions();
+
+          try {
+            const results = await getGeocode({ address });
+            const { lat, lng } = await getLatLng(results[0]);
+            // console.log(lat, lng);
+            panTo({ lat, lng });
+          } catch (error) {
+            console.log("error");
+          }
+          //   console.log(address);
+        }}
+      >
+        <ComboboxInput
+          className="input"
+          value={value}
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+          disabled={!ready}
+          placeholder="Search For Location"
+        />
+        <ComboboxPopover className="has-text-black">
+          {status === "OK" &&
+            data.map(({ id, description }) => (
+              <ComboboxOption key={id} value={description} />
+            ))}
+        </ComboboxPopover>
+      </Combobox>
+    </div>
   );
 }
